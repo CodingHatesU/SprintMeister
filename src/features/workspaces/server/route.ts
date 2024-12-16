215895df08b695ffeca601ce7d6e10c5f94531fd
CodingHatesU <adityatawade98@gmail.com>
2024-12-16 21:49:13 +0530

    halfway through handling image uploads 4:48:30

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,7 +2,7 @@ import { zValidator } from "@hono/zod-validator";
 import {Hono} from "hono";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 import { ID } from "node-appwrite";
 
 const app = new Hono()
@@ -12,9 +12,20 @@ const app = new Hono()
         sessionMiddleware,
         async (c) => {
             const databases = c.get("databases");
+            const storage = c.get("storage");
             const user = c.get("user");
 
-            const {name} = c.req.valid("json");
+            const {name, image} = c.req.valid("json");
+
+            let uploadedImageUrl: string | undefined;
+
+            if(image instanceof File) {
+                const file = await storage.createFile(
+                    IMAGES_BUCKET_ID,
+                    ID.unique(),
+                    image
+                )
+            }
 
             const workspace = await databases.createDocument(
                 DATABASE_ID,
